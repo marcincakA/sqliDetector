20e62e462d096068ca778bc706ddd7b9531c1e05
marcincakA <marcincaka@gmail.com>
2024-03-05 17:44:41 +0100

    initial

--- main.php
+++ main.php
@@ -3,31 +3,31 @@ include 'Classes/Tokenizer.php';
 include 'Classes/MyToken.php';
 $tokenizer = new Tokenizer('TestFile.php');
 $tokens = $tokenizer->getTokens();
-$vulnerable = array();
 $hashMapOfLines = array();
 
 foreach ($tokens as $token) {
     $lineNumber = $token->line;
     if ($lineNumber != $oldLineNumber) {
+        $hashMapOfLines[$lineNumber][] = $line; // store line in hashmap
         //new line created
         $line = new Line($lineNumber, false);
     }
     $oldLineNumber = $lineNumber;
 
     $isVulnerable = false;
+    //skip whitespace
     if ($token->id == 397){
         continue;
     }
-    if ($token->id == 319){
-        $vulnerable[] = $token;
+    //vulnerable if string with parameters or mysqli_query
+    if ($token->id == 319 || $token->text == 'mysqli_query'){
         $isVulnerable = true;
+        $line->setVulnerable(); //set vulnerable line to true
     }
+
     echo "Token text => " . " " . $token->text. "  Token id => " . $token->id . " Token name => " . token_name($token->id) . " line: ". $token->line . " pos: " . $token->pos ."<br>";
     echo "<br>";
-
-    $hashMapOfLines[$lineNumber][] = new MyToken($token, $isVulnerable); // store tokens in a hashmap
-
-
+    $line->addToken(new MyToken($token, $isVulnerable));
 }
 echo "<br>";
 ?>
