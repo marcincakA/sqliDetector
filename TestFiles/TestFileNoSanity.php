f34ee3513706ab8a57fbe639a43afc81740d80dd
marcincakA <marcincaka@gmail.com>
2024-04-19 14:14:04 +0200

    refactor plus mode selection

--- TestFiles/TestFileNoSanity.php
+++ TestFiles/TestFileNoSanity.php
@@ -2,4 +2,4 @@
 
 $id = $_GET['id'];
 $query = "SELECT * FROM products WHERE id=$id";
-$result = database->query("SELECT * FROM products WHERE id=".$id);
+$result = $database->query("SELECT * FROM products WHERE id=".$id);
